6be1dd87fcc362b6ac0f427f55e25c4b5a54996c
dabi2 <dabiangjalong3832gmail.com>
2024-02-02 12:57:43 +0530

    h

--- lib/Colors/Colors.dart
+++ lib/Colors/Colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class MainColors {
   static Color tileColors = Color.fromRGBO(255, 255, 255, 0.4);
-  static Color darkColor = Color.fromARGB(51, 1, 189, 178);
+  static Color darkColor = Color.fromARGB(204, 57, 163, 156);
   static Color brightColor = Color.fromARGB(255, 1, 189, 178);
   static Color lightColor = Color.fromARGB(177, 104, 180, 175);
-  static Color mainColor = Color.fromARGB(204, 57, 163, 156);
+  static Color mainColor = Color.fromARGB(51, 1, 189, 178);
 }
