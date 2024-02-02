1b5c1a39e216da38a94909ee50a90959d86fce06
2609Nahath <nankervis1125@gmail.com>
2024-02-02 12:50:34 +0530

    commit

--- lib/feedpage.dart
+++ lib/feedpage.dart
@@ -11,20 +11,14 @@ class FeedPage extends StatefulWidget {
 class _feedpageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(      
+    return Column(
       children: [
         Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Container(
-              decoration: BoxDecoration(
-                color: MainColors.darkColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-              ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              color: MainColors.lightColor,
               height: 200,
               child: Column(
                 children: [
@@ -35,61 +29,67 @@ class _feedpageState extends State<FeedPage> {
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Colors.green,
-                    ),                    
+                    ),
                   ),
                   const Padding(
                     padding: EdgeInsets.all(8.0),
-                    child: Text(                    
-                        'Complete your profile and work details and get best loan Offers ',textAlign: TextAlign.center,),
+                    child: Text(
+                      'Complete your profile and work details and get best loan Offers ',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(20.0),
                     child: ElevatedButton(
-                        onPressed: () { 
-                 
-                        },
-                        child: const Text('Check Loan Offers Up to \u20B950,000')),
+                        onPressed: () {},
+                        child:
+                            const Text('Check Loan Offers Up to \u20B950,000')),
                   ),
-                  SizedBox(                
-                    child: ElevatedButton(
-                      style: ButtonStyle(                  
-                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF02706B))
-                      ),
-                      
-                    onPressed: () {},
-                    child: const Text(
-                      'Know More',
-                    style: TextStyle( color: Colors.white,),)))
+                  SizedBox(
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Color(0xFF02706B))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Know More',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )))
                 ],
-              ),              
-            ),            
+              ),
+            ),
+          ),
         ),
         const SizedBox(
           child: Align(
-            alignment: Alignment(-1,-1),
-            child: Text('Your Credit Score',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            alignment: Alignment(-1, -1),
+            child: Text(
+              'Your Credit Score',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
         SizedBox(
           child: Container(
-            
             height: 200,
             decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage('images/coins.jpeg'
-              ),fit: BoxFit.fill,),
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
+              image: DecorationImage(
+                image: AssetImage('images/coins.jpeg'),
+                fit: BoxFit.fill,
               ),
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
           ),
         )
       ],
-      
     );
-        
   }
 }
