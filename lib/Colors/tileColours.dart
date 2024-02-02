7ef8d2db08988f1690aeb4989df654fd0294da3a
2609Nahath <nankervis1125@gmail.com>
2024-02-02 11:59:56 +0530

    Colors

---
+++ lib/Colors/tileColours.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static Color tileColors = const Color.fromRGBO(255, 255, 255, 0.4);
+  static Color darkColor = const Color.fromARGB(51, 1, 189, 178);
+  static Color brightColor = const Color.fromARGB(255, 1, 189, 178);
+  static Color lightColor = const Color.fromARGB(177, 104, 180, 175);
+  static Color mainColor = const Color.fromARGB(204, 57, 163, 156);
+}
